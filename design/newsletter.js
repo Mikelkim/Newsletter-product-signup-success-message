b9075c1af36470dae29ec17dac67818b3eb64ae0
Mikelkim <115950919+Mikelkim@users.noreply.github.com>
2023-08-07 20:30:01 +0300

    Frontend-project-1
    Any feedback is accepted and appreciated

---
+++ design/newsletter.js
@@ -0,0 +1,36 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   const submit = document.querySelector('button[type="submit"]');
+    const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+    submit.addEventListener('click', (event) => {
+      event.preventDefault();
+  
+      const email = document.getElementById('email').value;
+      if (email.match(valid)) {
+        window.location.assign("success.html");
+         return true;  
+
+      }
+  
+     else {
+        var email_error = document.getElementById("email_error");
+        email_error.style.display = "block";
+        return true;
+      }
+    });
+});
+let email_change=document.getElementById('email_change');
+email_change.textContent = email;
+
+
+
+
+
+  
+
+
+
+
